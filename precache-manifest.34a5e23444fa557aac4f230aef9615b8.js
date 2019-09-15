37474a78816bea3dd29ee45343692e7e140e2baa
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-15 21:55:30 +0000

    Deploy atk-ytp.plumbing to github.com/TKOaly/atk-ytp-prod.git:gh-pages

---
+++ precache-manifest.34a5e23444fa557aac4f230aef9615b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "5a755659fd61b8b3e4bf",
+    "url": "/static/js/main.5a755659.chunk.js"
+  },
+  {
+    "revision": "ec8ce3e1ef7032184a4f",
+    "url": "/static/js/2.ec8ce3e1.chunk.js"
+  },
+  {
+    "revision": "5a755659fd61b8b3e4bf",
+    "url": "/static/css/main.bbf03a91.chunk.css"
+  },
+  {
+    "revision": "aa079881a0410ac22cd4a8712ec9a275",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f083c4054f685143ad93709924e7bf7a.js"
+  "/precache-manifest.34a5e23444fa557aac4f230aef9615b8.js"
 );
 
 workbox.clientsClaim();
