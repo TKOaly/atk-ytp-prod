157e199d63a53254f44d68ce0d4a8ac55780ab98
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-13 10:16:05 +0000

    Deploy atk-ytp.plumbing to github.com/TKOaly/atk-ytp-prod.git:gh-pages

--- precache-manifest.8d62890e5df4941080b4540adc3979c8.js
+++ precache-manifest.8d62890e5df4941080b4540adc3979c8.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.42ac5946.js"
   },
   {
-    "revision": "f733c87e4eee6047b21e",
-    "url": "/static/js/main.f733c87e.chunk.js"
+    "revision": "bf06eabdfe7b5614a181",
+    "url": "/static/js/main.bf06eabd.chunk.js"
   },
   {
     "revision": "ec8ce3e1ef7032184a4f",
     "url": "/static/js/2.ec8ce3e1.chunk.js"
   },
   {
-    "revision": "f733c87e4eee6047b21e",
-    "url": "/static/css/main.8d08e8b8.chunk.css"
+    "revision": "bf06eabdfe7b5614a181",
+    "url": "/static/css/main.73132692.chunk.css"
   },
   {
-    "revision": "5f5ea88ae0f2aba4bc1890264f59e3e8",
+    "revision": "4d39df69d25f586ce2030c45b480b346",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.797089d44d87ed5ef8af6cffe973ffa0.js"
+  "/precache-manifest.8d62890e5df4941080b4540adc3979c8.js"
 );
 
 workbox.clientsClaim();
